57bf3af9865fb0617c2983d8e2fb0ea6f781eb2b
sp41mer <sp41mer@yahoo.com>
2016-08-25 21:38:24 +0300

    detail method full done

--- js/index.js
+++ js/index.js
@@ -25,20 +25,32 @@ $(document).ready(function(){
   });
   $(".categories").on('click','.category.passive',function(){
     $(this).removeClass("passive").addClass("active");
+    if ($(this).data('level') == 1) $(".detail").remove();
     parents = $("tr[data-parent='"+$(this).data('id')+"']")
     $("tr[data-parent='"+$(this).data('id')+"']").remove();
     parents.each(function(index) {
       $("tr[data-parent='"+$(this).data('id')+"']").remove();
     });
   });
-  $(".categories").on('click','.answer',function(){
+  $(".categories").on('click','.answer.active',function(){
+    newThis = $(this);
     $(this).removeClass("active").addClass("passive");
     $.post("http://127.0.0.1:8888/method/getDetail",
         {'id': $(this).data('id'), 'version': '1.0'},
         function (data) {
-          text = data.videourl;
-          alert(text);
+          name = data.name;
+          text = data.text;
+          video = data.videourl;
+          image = data.imgurl;
+          newThis.after("<tr class = \'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Наименование</td><td>"+name+"</td></tr>" +
+              "<tr class =\'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Текст ответа</td><td>"+text+"</td></tr>" +
+              "<tr class =\'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Ссылка на видео</td><td>"+video+"</td></tr>" +
+              "<tr class =\'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Ссылка на изображение</td><td>"+image+"</td></tr>");
         }, "json");
+  });
+  $(".categories").on('click','.answer.passive',function(){
+    $(this).removeClass("passive").addClass("active");
+    $("tr[data-parent='"+$(this).data('id')+"']").remove();
   });
 
 });
