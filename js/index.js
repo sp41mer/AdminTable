9a28c5059330959f0cedca237490a62e1c0b8cdf
sp41mer <sp41mer@yahoo.com>
2016-08-26 11:02:10 +0300

    changed tables to divs

--- js/index.js
+++ js/index.js
@@ -3,11 +3,12 @@ $.post("http://127.0.0.1:8888/method/getContent",
     function (data) {
       responseCategories = (data.categories)
       responseCategories.forEach(function (item, i, arr) {
-        $(".categories > tbody:last-child").append("<tr class = \'category active level1\' data-id =\'" + item.id + "\' data-level ='1'><td>" + item.name + "</td></tr>");
+        $(".container").append("<div class = \'category active level1\' data-id =\'" + item.id + "\' data-level ='1'>" + item.name + "</div>");
       });
     }, "json");
 $(document).ready(function(){
-  $(".categories").on('click','.category.active',function(){
+  $(".container").on('click','.category.active',function(e){
+    e.stopPropagation();
     $(this).removeClass("active").addClass("passive");
     newThis = $(this);
     $.post("http://127.0.0.1:8888/method/getContent",
@@ -16,23 +17,20 @@ $(document).ready(function(){
           responseCategories = (data.categories);
           responseAnswers = (data.answers);
           responseCategories.forEach(function (item, i, arr) {
-            newThis.after("<tr  class = \'category active level2 \' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><td>" + item.name + "</td></tr>");
+            newThis.append("<div  class = \'category active level2 \' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'>" + item.name + "</div>");
           });
           responseAnswers.forEach(function (item, i, arr) {
-            newThis.after("<tr  class = \'answer active\' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><td>" + item.name + "</td></tr>");
+            newThis.append("<div  class = \'answer active\' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'>" + item.name + "</div>");
           });
         }, "json");
   });
-  $(".categories").on('click','.category.passive',function(){
+  $(".container").on('click','.category.passive',function(e){
+    e.stopPropagation();
     $(this).removeClass("passive").addClass("active");
-    if ($(this).data('level') == 1) $(".detail").remove();
-    parents = $("tr[data-parent='"+$(this).data('id')+"']")
-    $("tr[data-parent='"+$(this).data('id')+"']").remove();
-    parents.each(function(index) {
-      $("tr[data-parent='"+$(this).data('id')+"']").remove();
-    });
+    $(this).children().remove();
   });
-  $(".categories").on('click','.answer.active',function(){
+  $(".container").on('click','.answer.active',function(e){
+    e.stopPropagation();
     newThis = $(this);
     $(this).removeClass("active").addClass("passive");
     $.post("http://127.0.0.1:8888/method/getDetail",
@@ -42,15 +40,16 @@ $(document).ready(function(){
           text = data.text;
           video = data.videourl;
           image = data.imgurl;
-          newThis.after("<tr class = \'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Наименование</td><td>"+name+"</td></tr>" +
-              "<tr class =\'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Текст ответа</td><td>"+text+"</td></tr>" +
-              "<tr class =\'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Ссылка на видео</td><td>"+video+"</td></tr>" +
-              "<tr class =\'detail\' data-parent = \'"+newThis.data('id')+"\'><td>Ссылка на изображение</td><td>"+image+"</td></tr>");
+          newThis.append("<div class = \'detail\' data-parent = \'"+newThis.data('id')+"\'>Наименование"+name+"</div>" +
+              "<div class =\'detail\' data-parent = \'"+newThis.data('id')+"\'>Текст ответа"+text+"</div>" +
+              "<div class =\'detail\' data-parent = \'"+newThis.data('id')+"\'>Ссылка на видео"+video+"</div>" +
+              "<div class =\'detail\' data-parent = \'"+newThis.data('id')+"\'>Ссылка на изображение"+image+"</div>");
         }, "json");
   });
-  $(".categories").on('click','.answer.passive',function(){
+  $(".container").on('click','.answer.passive',function(e){
+    e.stopPropagation();
     $(this).removeClass("passive").addClass("active");
-    $("tr[data-parent='"+$(this).data('id')+"']").remove();
+    $(this).children().remove();
   });
 
 });
