f55f6a7d5cea72ecac65b52d4718360e081e4862
sp41mer <sp41mer@yahoo.com>
2016-08-26 11:43:19 +0300

    editable answers

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $.post("http://127.0.0.1:8888/method/getContent",
     function (data) {
       responseCategories = (data.categories)
       responseCategories.forEach(function (item, i, arr) {
-        $(".container").append("<div class = \'category active level1\' data-id =\'" + item.id + "\' data-level ='1'>" + item.name + "</div>");
+        $(".container").append("<div class = \'category active level1\' data-id =\'" + item.id + "\' data-level ='1'><span>" + item.name + "</span></div>");
       });
     }, "json");
 $(document).ready(function(){
@@ -17,10 +17,10 @@ $(document).ready(function(){
           responseCategories = (data.categories);
           responseAnswers = (data.answers);
           responseCategories.forEach(function (item, i, arr) {
-            newThis.append("<div  class = \'category active level2 \' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'>" + item.name + "</div>");
+            newThis.append("<div  class = \'category active level2 \' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><span>" + item.name + "</span></div>");
           });
           responseAnswers.forEach(function (item, i, arr) {
-            newThis.append("<div  class = \'answer active\' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'>" + item.name + "</div>");
+            newThis.append("<div  class = \'answer active\' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><span>" + item.name + "</span></div>");
           });
         }, "json");
   });
@@ -40,16 +40,23 @@ $(document).ready(function(){
           text = data.text;
           video = data.videourl;
           image = data.imgurl;
-          newThis.append("<div class = \'detail\' data-parent = \'"+newThis.data('id')+"\'>Наименование"+name+"</div>" +
-              "<div class =\'detail\' data-parent = \'"+newThis.data('id')+"\'>Текст ответа"+text+"</div>" +
-              "<div class =\'detail\' data-parent = \'"+newThis.data('id')+"\'>Ссылка на видео"+video+"</div>" +
-              "<div class =\'detail\' data-parent = \'"+newThis.data('id')+"\'>Ссылка на изображение"+image+"</div>");
+          newThis.append("<table class = \'details\'><thead><td>Название</td><td>Текст</td><td>Ccылка на видео</td><td>Изображение</td></thead>" +
+              "<tbody><tr><td contenteditable=\'true\' class = \'detail name\' data-parent = \'"+newThis.data('id')+"\'>"+name+"</td>" +
+              "<td contenteditable=\'true\' class =\'detail text\' data-parent = \'"+newThis.data('id')+"\'>"+text+"</td>" +
+              "<td contenteditable=\'true\' class =\'detail video\' data-parent = \'"+newThis.data('id')+"\'>"+video+"</td>" +
+              "<td contenteditable=\'true\' class =\'detail image\' data-parent = \'"+newThis.data('id')+"\'>"+image+"</td></tr></tbody></table><button class=\'js__details__button__save\'>Сохранить</button>");
         }, "json");
   });
   $(".container").on('click','.answer.passive',function(e){
     e.stopPropagation();
     $(this).removeClass("passive").addClass("active");
     $(this).children().remove();
+  });
+  $(".container").on('click','.details',function(e){
+    e.stopPropagation();
+  });
+  $(".container").on('click','.js__details__button__save',function(e){
+    e.stopPropagation();
   });
 
 });
