211604d37a69cd280b0397fcc131542235a3f7f3
sp41mer <sp41mer@yahoo.com>
2016-08-26 23:18:27 +0300

    categories buttons

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $.post("http://127.0.0.1:8888/method/getContent",
     function (data) {
       responseCategories = (data.categories)
       responseCategories.forEach(function (item, i, arr) {
-        $(".container").append("<div class = \'category active level1\' data-id =\'" + item.id + "\' data-level ='1'><span>" + item.name + "</span><button class = \'js__category__edit\'>Редактировать</button><button class = \'js__category__remove\'>Удалить</button></div>");
+        $(".container").append("<div class = \'category active level1\' data-id =\'" + item.id + "\' data-level ='1'><span>" + item.name + "</span><button class = \'js__category__button__edit\'>Редактировать</button><button class = \'js__category__button__cancel\'>Отменить</button><button class = \'js__category__button__save\'>Сохранить</button><button class = \'js__category__button__remove\'>Удалить</button></div>");
       });
     }, "json");
 $(document).ready(function(){
@@ -17,17 +17,20 @@ $(document).ready(function(){
           responseCategories = (data.categories);
           responseAnswers = (data.answers);
           responseCategories.forEach(function (item, i, arr) {
-            newThis.append("<div  class = \'category active level2 \' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><span>" + item.name + "</span><button class = \'js__category__edit\'>Редактировать</button><button class = \'js__category__remove\'>Удалить</button></div>");
+            newThis.append("<div  class = \'category active level2 \' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><span>" + item.name + "</span><button class = \'js__category__button__edit\'>Редактировать</button><button class = \'js__category__button__cancel\'>Отменить</button><button class = \'js__category__button__save\'>Сохранить</button><button class = \'js__category__button__remove\'>Удалить</button></div>");
           });
           responseAnswers.forEach(function (item, i, arr) {
-            newThis.append("<div  class = \'answer active\' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><span>" + item.name + "</span><button class = \'js__category__edit\'>Редактировать</button><button class = \'js__category__remove\'>Удалить</button></div>");
+            newThis.append("<div  class = \'answer active\' data-level =\'" + (newThis.data('level')+1) + "\' data-id =\'" + item.id + "\' data-parent=\'"+newThis.data('id')+"\'><span>" + item.name + "</span><button class = \'js__category__button__edit\'>Редактировать</button><button class = \'js__category__button__cancel\'>Отменить</button><button class = \'js__category__button__save\'>Сохранить</button><button class = \'js__category__button__remove\'>Удалить</button></div>");
           });
+          newThis.append("<button class = \'js__category__button__add\'>Добавить</button>");
+
+
         }, "json");
   });
   $(".container").on('click','.category.passive',function(e){
     e.stopPropagation();
     $(this).removeClass("passive").addClass("active");
-    $(this).children().slice(1).remove();
+    $(this).children().slice(5).remove();
   });
   $(".container").on('click','.answer.active',function(e){
     e.stopPropagation();
@@ -50,7 +53,7 @@ $(document).ready(function(){
   $(".container").on('click','.answer.passive',function(e){
     e.stopPropagation();
     $(this).removeClass("passive").addClass("active");
-    $(this).children().slice(1).remove();
+    $(this).children().slice(5).remove();
   });
   $(".container").on('click','.details',function(e){
     e.stopPropagation();
@@ -58,5 +61,26 @@ $(document).ready(function(){
   $(".container").on('click','.js__details__button__save',function(e){
     e.stopPropagation();
   });
+
+  //categories buttons
+  $(".container").on('click','.js__category__button__edit',function(e){
+    e.stopPropagation();
+    $(this).parent().find('span').attr('contenteditable','true');
+  });
+  $(".container").on('click','.js__category__button__save',function(e){
+    e.stopPropagation();
+    $(this).parent().find('span').removeAttr('contenteditable','true');
+  });
+  $(".container").on('click','.js__category__button__remove',function(e){
+    e.stopPropagation();
+  });
+  $(".container").on('click','.js__category__button__cancel',function(e){
+    e.stopPropagation();
+    $(this).parent().find('span').removeAttr('contenteditable','true');
+  });
+  $(".container").on('click','.js__category__button__add',function(e){
+    e.stopPropagation();
+  });
+
 
 });
